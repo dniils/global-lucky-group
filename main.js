b0dd7c99a4198a486cee7e9160d67da53a6e4b07
Daniil Shitikov <tochka098@gmail.com>
2023-12-08 12:21:15 +0300

    feat: fetch description from baconipsum

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 import './assets/style/main.scss';
 import './burger';
+
+const descriptionDivElement = document.querySelector('.module__description');
+
+async function getDescription(numberOfSentences) {
+  const response = await fetch(
+    `https://baconipsum.com/api/?type=lucky?sentences=${numberOfSentences}`
+  );
+  const data = await response.json();
+  return data[0];
+}
+
+const description = await getDescription(3);
+descriptionDivElement.innerHTML = description;
